efdeaa05f588ce901dcb91df3877f423dc024ec8
Sumaid Syed <sumaid.ali@students.iiit.ac.in>
2019-04-19 18:23:37 +0530

    Copier : Sort files

--- copier.py
+++ copier.py
@@ -12,8 +12,8 @@ dstdir="./full_test/"
 imagecount = 0
 pptcount = 1
 f = open('datasetoutput.txt', "w")
-for filename in os.listdir(os.getcwd()+'/Dataset/'):
-    for image in os.listdir(str(os.getcwd()+'/Dataset/'+filename)):
+for filename in sorted(os.listdir(os.getcwd()+'/Dataset/')):
+    for image in sorted(os.listdir(str(os.getcwd()+'/Dataset/'+filename))):
         if str(image) == "ppt.jpg":
             shutil.copy2(str(os.getcwd()+'/Dataset/'+filename+'/'+image), str(dstdir+'slides/'+'ppt'+str(pptcount)+'.jpg'))
             pptcount = pptcount + 1
