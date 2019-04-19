3cfab3865b178363d335d4fea04eb67ff70479a8
AadilMehdi Sanchawala <aadilmehdi.s@students.iiit.ac.in>
2019-04-19 20:01:36 +0530

    Sumaid's attempts

--- move.py
+++ move.py
@@ -11,6 +11,7 @@ if not os.path.exists("./full_test"):
     os.mkdir("./full_test/frames")
 
 DEST_DIR = "./full_test/"
+f =  open('datasetoutput.txt', 'w+')
 
 test_count = 1
 ppt_count = 1
@@ -18,9 +19,12 @@ for folder in sorted(os.walk('./Dataset')):
     for file in folder[2]:
         file_src = folder[0] + "/" + file
         if 'ppt' in file:
-            file_dest = DEST_DIR + "frames/" + folder[0].split('/')[-1] + "_ppt" + str(ppt_count) + '.jpg'
+            file_dest = DEST_DIR + "slides/" + folder[0].split('/')[-1] + "_ppt" + str(ppt_count) + '.jpg'
             ppt_count += 1
+
         else:
             file_dest = DEST_DIR + "frames/" + folder[0].split('/')[-1] + "_"+ str(test_count) + '.jpg'
+            f.write(folder[0].split('/')[-1] + "_"+ str(test_count) + '.jpg     ' + folder[0].split('/')[-1] + "_ppt" + str(ppt_count) + '.jpg\n')
             test_count += 1
         shutil.copyfile(file_src, file_dest)
+        
